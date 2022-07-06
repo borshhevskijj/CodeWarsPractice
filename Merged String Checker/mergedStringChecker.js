826a5b24e91be24e910d2c17527c08ae6287d0e6
borshhevskijj <borshhevskijj98@mail.ru>
2022-07-06 09:44:27 +0300

    Merged String Checker

--- Merged String Checker/mergedStringChecker.js
+++ Merged String Checker/mergedStringChecker.js
@@ -1,19 +1,3 @@
-// function isMerge(s, part1, part2) {
-//   if (s.length !== part1.length + part2.length) {
-//     return "не содержит"
-//   }
-//   if (!s.length) {
-//     return "содержит"
-//   }
-//   if (part1[0] === s[0] && isMerge(s.slice(1), part1.slice(1), part2)) {
-//     return "содержит"
-//   }
-//   if (part2[0] === s[0] && isMerge(s.slice(1), part1, part2.slice(1))) {
-//     return "содержит"
-//   }
-//   return "не содержит"
-// }
-
 function isMerge(s, part1, part2) {
   if (!s.length && !part1.length && !part2.length) {
     return true
@@ -30,5 +14,3 @@ function isMerge(s, part1, part2) {
   }
   return false
 }
-
-console.log(isMerge("codewars", "code", "wars"))
