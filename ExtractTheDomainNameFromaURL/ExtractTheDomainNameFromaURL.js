e46234eb45e48d20f98799c692d03b5b89671599
borshhevskijj <borshhevskijj98@mail.ru>
2022-07-15 21:26:43 +0300

    Pete, The Baker

--- ExtractTheDomainNameFromaURL/ExtractTheDomainNameFromaURL.js
+++ ExtractTheDomainNameFromaURL/ExtractTheDomainNameFromaURL.js
@@ -5,8 +5,8 @@ function domainName(url) {
   return result[0]
 }
 
-console.log(domainName("https://www.2ev8iq5f4d1f.fr/default.html"))
-console.log(domainName("http://google.com"))
-console.log(domainName("http://google.co.jp"))
-console.log(domainName("http://www.google.co.jp"))
-console.log(domainName("https://www.ayc7bz2jdm-fdnmwy467rz657f.org"))
+// console.log(domainName("https://www.2ev8iq5f4d1f.fr/default.html"))
+// console.log(domainName("http://google.com"))
+// console.log(domainName("http://google.co.jp"))
+// console.log(domainName("http://www.google.co.jp"))
+// console.log(domainName("https://www.ayc7bz2jdm-fdnmwy467rz657f.org"))
---
+++ PeteTheBaker/peteTheBaker.js
@@ -0,0 +1,25 @@
+function cakes(recipe, available) {
+  const recipeArr = Object.entries(recipe)
+  const availableArr = Object.entries(available)
+  const result = []
+
+  for (let item in recipe) {
+    if (recipe[item] === undefined || available[item] === undefined) {
+      return 0
+    } else if (`${recipeArr.includes(availableArr[item])}`) {
+      result.push(Math.floor(available[item] / recipe[item]))
+    }
+  }
+
+  return Math.min(...result)
+}
+
+let recipe = { apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }
+let available = { sugar: 500, flour: 2000, milk: 2000 }
+// console.log(cakes(recipe, available))
+console.log(
+  cakes(
+    { flour: 500, sugar: 200, eggs: 1 },
+    { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+  )
+)
