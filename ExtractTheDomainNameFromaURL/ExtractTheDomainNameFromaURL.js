798cd2ad12952720fb4ae6503b496a1feaf41d05
borshhevskijj <borshhevskijj98@mail.ru>
2022-07-15 15:40:36 +0300

    Extract the domain name from a URL

---
+++ ExtractTheDomainNameFromaURL/ExtractTheDomainNameFromaURL.js
@@ -0,0 +1,12 @@
+function domainName(url) {
+  const filtredURL = url.replace(/^(?:https?:\/\/)?(www\.)?/, "")
+  const regExp = /[^\.]*/
+  const result = filtredURL.match(regExp)
+  return result[0]
+}
+
+console.log(domainName("https://www.2ev8iq5f4d1f.fr/default.html"))
+console.log(domainName("http://google.com"))
+console.log(domainName("http://google.co.jp"))
+console.log(domainName("http://www.google.co.jp"))
+console.log(domainName("https://www.ayc7bz2jdm-fdnmwy467rz657f.org"))
---
+++ ExtractTheDomainNameFromaURL/README.md
@@ -0,0 +1,15 @@
+task link:
+
+---
+
+https://www.codewars.com/kata/514a024011ea4fb54200004b
+
+task description:
+
+---
+
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+- url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+- url = "http://www.zombie-bites.com" -> domain name = "zombie-bites"
+- url = "https://www.cnet.com" -> domain name = "cnet"
