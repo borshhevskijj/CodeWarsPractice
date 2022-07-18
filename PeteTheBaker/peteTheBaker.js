aefbdfbe8d3af48b5a486f00c0f56f041b925a09
borshhevskijj <borshhevskijj98@mail.ru>
2022-07-18 19:09:35 +0300

    Array.diff

--- PeteTheBaker/peteTheBaker.js
+++ PeteTheBaker/peteTheBaker.js
@@ -1,6 +1,6 @@
 function cakes(recipe, available) {
-  const recipeArr = Object.entries(recipe)
-  const availableArr = Object.entries(available)
+  const recipeArr = Object.keys(recipe)
+  const availableArr = Object.keys(available)
   const result = []
 
   for (let item in recipe) {
@@ -14,12 +14,13 @@ function cakes(recipe, available) {
   return Math.min(...result)
 }
 
-let recipe = { apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }
-let available = { sugar: 500, flour: 2000, milk: 2000 }
-// console.log(cakes(recipe, available))
-console.log(
-  cakes(
-    { flour: 500, sugar: 200, eggs: 1 },
-    { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
-  )
-)
+// let recipe = { apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }
+// let available = { sugar: 500, flour: 2000, milk: 2000 }
+// // console.log(cakes(recipe, available))
+
+// console.log(
+//   cakes(
+//     { flour: 500, sugar: 200, eggs: 1 },
+//     { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+//   )
+// )
