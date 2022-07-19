df404509263abc09724b30c6e9b8d3f4c4157003
borshhevskijj <borshhevskijj98@mail.ru>
2022-07-19 21:05:56 +0300

    Sum of Digits / Digital Root

--- ArrayDiff/Array.diff.js
+++ ArrayDiff/Array.diff.js
@@ -3,19 +3,3 @@ function arrayDiff(a, b) {
 }
 
 // console.log(arrayDiff([1, 2, 3], [1, 2])) // [3]
-
-function likes(names) {
-  if (names.length === 0) {
-    return "no one likes this"
-  } else if (names.length === 1) {
-    return `${names[0]} likes this`
-  } else if (names.length === 2) {
-    return `${names[0]} and ${names[1]} likes this`
-  } else if (names.length === 3) {
-    return `${names[0]},${names[1]} and ${names[2]} like this`
-  }
-  return `${names[0]},${names[1]} and ${names.length - 3} like this`
-}
-
-// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
-console.log(likes(["Max", "John", "Mark"]))
---
+++ Sum of Digits or Digital Root/SumOfDigitsOrDigitalRoot.js
@@ -0,0 +1,11 @@
+function digital_root(n) {
+  const splitedNumber = String(n)
+    .split("")
+    .reduce((total, current) => total + Number(current), 0)
+  //   if (String(splitedNumber).split("").length > 1) {
+  if (splitedNumber > 10) {
+    return digital_root(splitedNumber)
+  }
+  return splitedNumber
+}
+console.log(digital_root(456))
